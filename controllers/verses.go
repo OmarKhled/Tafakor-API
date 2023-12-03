326a917ce6858a48e0e472bd0444a8225436a1ed
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2023-12-03 06:15:16 +0200

    feature: verses query

--- controllers/verses.go
+++ controllers/verses.go
@@ -2,17 +2,52 @@ package verses
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
-	"tafakor.app/migrate"
 )
+
+type response (struct {
+	ID          string         `json:"id"`
+	SurahNumber int            `json:"surah_number"`
+	From        int            `json:"from"`
+	To          int            `json:"to"`
+	Count       int            `json:"count"`
+	LastPublish sql.NullString `json:"last_publish"`
+})
 
 func GetVerses(db *sql.DB) func(c *fiber.Ctx) error {
 
 	return func(c *fiber.Ctx) error {
-		migrate.Seed(db)
+		var verses []response
 
-		return c.JSON(migrate.FetchPosts())
+		rows, err := db.Query(`
+			SELECT id, surah_number, start, "end",
+				(SELECT count(id) FROM post WHERE V.id = post.verse) as count,
+				(SELECT max(created_at) FROM post WHERE V.id = post.verse) as last_publish
+			FROM verse as V
+			ORDER BY count, last_publish DESC, created_at DESC;
+		`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+
+			var verse response
+			if err := rows.Scan(&verse.ID, &verse.SurahNumber, &verse.From, &verse.To, &verse.Count, &verse.LastPublish); err != nil {
+				log.Fatal(err)
+				return c.JSON(verses)
+			}
+			fmt.Println("verse", verse)
+			verses = append(verses, verse)
+		}
+
+		fmt.Println(verses)
+
+		return c.JSON(verses)
 	}
 
 }
