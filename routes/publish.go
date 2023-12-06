466c70d87b1b6f0a5edd45c7ebb36be572226cc0
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2023-12-06 03:56:59 +0200

    feature: publishing routes

--- routes/publish.go
+++ routes/publish.go
@@ -4,43 +4,102 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"os"
+
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	controllers "tafakor.app/controllers"
+	"tafakor.app/utils"
 )
 
 type publishmentBody struct {
-	PostingType   string `json:"posting_type"`
-	FileURL       string `json:"file_url"`
-	VerseID       string `json:"verse_id"`
-	StockID       string `json:"stock_id"`
-	StockProvider string `json:"stock_provider"`
+	PostingType   string `query:"posting_type" json:"posting_type"`
+	FileURL       string `query:"file_url" json:"file_url"`
+	VerseID       string `query:"verse_id" json:"verse_id"`
+	StockID       string `query:"stock_id" json:"stock_id"`
+	StockProvider string `query:"stock_provider" json:"stock_provider"`
 }
 
 func PublishRoutes(group fiber.Router, db *sql.DB) {
 	group.Post("/", func(c *fiber.Ctx) error {
+		// Enviroment Variables
+		SENDER_EMAIL := os.Getenv("SENDER_EMAIL")
+		SENDER_PASS := os.Getenv("SENDER_PASS")
+		EMAIL_HOST := os.Getenv("EMAIL_HOST")
+		PORT := os.Getenv("PORT")
+		SUPERVISOR_EMAIL := os.Getenv("PORT")
+		TAFAKOR_ENDPOINT := os.Getenv("TAFAKOR_ENDPOINT")
+
 		var body publishmentBody
 		json.Unmarshal(c.Body(), &body)
 
-		status, id := controllers.PublishToFB(body.PostingType, body.FileURL)
+		parameters := fmt.Sprintf("?posting_type=%v&file_url=%v&verse_id=%v&stock_id=%v&stock_provider=%v", body.PostingType, body.FileURL, body.VerseID, body.StockID, body.StockProvider)
 
-		fmt.Println(status, id)
+		acceptLink := fmt.Sprintf("%v/accept", TAFAKOR_ENDPOINT) + parameters
+		rejectLink := fmt.Sprintf("%v/reject", TAFAKOR_ENDPOINT) + parameters
+		rerenderLink := fmt.Sprintf("%v/reject-stock", TAFAKOR_ENDPOINT) + parameters
+
+		r := strings.NewReplacer("|POST-LINK|", body.FileURL, "|ACCEPT-LINK|", acceptLink, "|REJECT-LINK|", rejectLink, "|REJECT-RENDER-LINK|", rerenderLink)
+
+		template, _ := os.ReadFile("templates/approval.html")
+
+		emailBody := r.Replace(string(template))
+
+		utils.SendMail(SENDER_EMAIL, SENDER_PASS, EMAIL_HOST, PORT, SUPERVISOR_EMAIL, "منشور جديد قيد الموافقة", emailBody)
+
+		return c.JSON(nil)
+	})
+
+	group.Get("/accept", func(c *fiber.Ctx) error {
+		var parameters publishmentBody
+		c.QueryParser(&parameters)
+
+		status, id := controllers.PublishToFB(parameters.PostingType, parameters.FileURL)
 
 		if status == true {
-			fmt.Println("tany")
-			query := "INSERT INTO post(verse, published, footage, footageid) VALUES( $1, $2, $3, $4 )"
-			insert, err := db.Prepare(query)
+			postQuery := "INSERT INTO post(verse, published, state, footage, postid) VALUES( $1, $2, $3, $4, $5 ) RETURNING id"
+			postInsert, _ := db.Prepare(postQuery)
 
-			if err != nil {
-				fmt.Println(err)
-			}
+			var postId string
+			postInsert.QueryRow(parameters.VerseID, true, "accepted", parameters.FileURL, id).Scan(postId)
 
-			resp, err := insert.Exec(body.VerseID, true, body.FileURL, string(id))
-			fmt.Println(resp, err)
+			stockQuery := "INSERT INTO stock_footage(id, post, provider, state) VALUES( $1, $2, $3, $4 )"
+			stockInsert, _ := db.Prepare(stockQuery)
 
-			insert.Close()
+			stockInsert.Exec(parameters.StockID, postId, parameters.StockProvider, "accepted")
+
+			postInsert.Close()
+			stockInsert.Close()
+
+			return c.JSON(id)
+		} else {
+			return c.JSON(nil)
 		}
-
-		return c.JSON(status)
 	})
+
+	group.Get("/reject", func(c *fiber.Ctx) error {
+		var parameters publishmentBody
+		c.QueryParser(&parameters)
+
+		postQuery := "INSERT INTO post(verse, published, state, footage, postid) VALUES( $1, $2, $3, $4, $5 ) RETURNING id"
+		postInsert, _ := db.Prepare(postQuery)
+
+		var postId string
+		postInsert.QueryRow(parameters.VerseID, true, "rejected", parameters.FileURL, nil).Scan(postId)
+
+		stockQuery := "INSERT INTO stock_footage(id, post, provider, state) VALUES( $1, $2, $3, $4 )"
+		stockInsert, _ := db.Prepare(stockQuery)
+
+		stockInsert.Exec(parameters.StockID, postId, parameters.StockProvider, "rejected")
+
+		postInsert.Close()
+		stockInsert.Close()
+
+		return c.JSON(true)
+	})
+
+	// group.Get("/reject-stock", func(c *fiber.Ctx) error {
+
+	// })
 }
---
+++ utils/send-mail.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"fmt"
+	"net/smtp"
+)
+
+// Sends mails given sender and reciever
+func SendMail(senderMail string, senderPass string, host string, port string, recieverEmail string, subject string, body string) error {
+
+	// Set up authentication information.
+	auth := smtp.PlainAuth("", senderMail, senderPass, host)
+
+	// Array of recievers
+	recipientList := []string{recieverEmail}
+
+	// Email headers - RFC 822 Message Format
+	recieverHeader := fmt.Sprintf("To: %v\r\n", recieverEmail)
+	subjectHeader := fmt.Sprintf("Subject: %v!\r\n", subject)
+	MIMEHeader := fmt.Sprintf("MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\r\n")
+	content := body
+
+	msg := []byte(recieverHeader + subjectHeader + MIMEHeader + "\r\n" + content)
+	err := smtp.SendMail(host+":"+port, auth, senderMail, recipientList, msg)
+
+	return err
+}
